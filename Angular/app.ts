a409558d32b39f4e610709bdcc7a660c6506e889
andmenegaz <andre.menegaz@litma.com.br>
2020-05-12 14:10:38 -0300

    Packages Updates

--- Angular/app.ts
+++ Angular/app.ts
@@ -1,7 +1,7 @@
 import {MillenniumFalcon} from './starfighters'
 import {Spacecraft, Containership} from './base-ships'
 import * as _ from 'lodash'
-
+import { readFileSync } from 'fs';
 console.log(_.pad("Typescript Examples", 40, "="))
 
 let ship = new Spacecraft('hyperdrive')
@@ -11,4 +11,8 @@ let falcon = new MillenniumFalcon();
 falcon.jumpIntoHyperSpace();
 
 let goodForTheJob = (ship: Containership) => ship.cargoContainers >2
-console.log(`Is falcon good for the job? ${goodForTheJob(falcon) ? 'YES' : 'NO'}`);
+console.log(`Is falcon good for the job? ${goodForTheJob(falcon) ? 'YES' : 'NO'}`);[]
+
+const file = readFileSync("./teste.json", 'utf-8')
+let json = JSON.parse(file)
+console.log(json.sucess)
